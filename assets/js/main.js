339e71550756932d453ad1c3e0a16b57b079e245
Cezar <franga.cezar@gmail.com>
2017-01-14 09:15:29 +0200

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// BROWSER DETECTION
+$(document).ready(function () {
+    //$(function () {
+    //    SyntaxHighlighter.all();
+    //});
+    if (navigator.userAgent.indexOf('Mac OS X') != -1) {
+        // Mac
+        if ($.browser.opera) { $('html').addClass('opera'); }
+        if ($.browser.webkit) { $('html').addClass('webkit'); }
+        if ($.browser.mozilla) { $('html').addClass('mozilla'); }
+        if (/camino/.test(navigator.userAgent.toLowerCase())) { $('html').addClass('camino'); }
+        if (/chrome/.test(navigator.userAgent.toLowerCase())) { $('html').addClass('chrome'); }
+        if (navigator && navigator.platform && navigator.platform.match(/^(iPad|iPod|iPhone)$/)) { $('html').addClass('apple'); }
+        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) { $('html').addClass('safari'); }
+    }
+    else {
+        // Not Mac
+        if ($.browser.opera) { $('html').addClass('opera-pc'); }
+        if ($.browser.webkit) { $('html').addClass('webkit-pc'); }
+        if ($.browser.mozilla) { $('html').addClass('mozilla-pc'); }
+        if (document.all && document.addEventListener) { $('html').addClass('ie9'); }
+        if (/chrome/.test(navigator.userAgent.toLowerCase())) { $('html').addClass('chrome-pc'); }
+        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) { $('html').addClass('safari-pc'); }
+    }
+   
+});
+// END BROWSER DETECTION
